d185b5bd86baefb6611224e0e01dd5499f344773
BTARU <87946133+BTARU@users.noreply.github.com>
2021-10-20 21:10:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+"use strict"
+
+//  Burger menu
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
+
+// Прокрутка по клику
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+// Прокрутка по клику
+//  Burger menu
+
+// jquery slider
+$(document).ready(function(){
+    $('.twitter-slider__body').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+    });
+});
+// jquery slider
